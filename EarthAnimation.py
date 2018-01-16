29178b3e27a15489dbcc7e15ba227af39c91fe78
Rob <robert@nessdesign.biz>
2018-01-16 11:20:47 +0000

    added more images to animation

--- EarthAnimation.py
+++ EarthAnimation.py
@@ -13,11 +13,11 @@ O = (0,0,0)
 earthAnimation_1 = [
     O, O, O, O, O, O, O, O,
     O, B, W, W, W, G, O, O,
-    G, B, B, W, W, B, G, O,
+    G, B, B, W, W, B, B, O,
     B, G, B, G, B, G, G, O,
     G, G, B, B, G, B, G, O,
-    B, B, G, B, B, B, G, O,
-    O, W, W, W, B, G, O, O,
+    B, B, G, B, B, B, B, O,
+    O, W, W, W, B, B, O, O,
     O, O, O, O, O, O, O, O,
     ]
     
@@ -54,6 +54,83 @@ earthAnimation_4 = [
     O, O, O, O, O, O, O, O,
     ]
 
+earthAnimation_5 = [
+    O, O, O, O, O, O, O, O,
+    O, G, W, G, G, B, O, O,
+    B, G, B, B, G, B, B, O,
+    G, G, B, B, B, G, B, O,
+    G, B, B, B, G, G, B, O,
+    B, B, B, G, B, B, G, O,
+    O, B, W, B, B, W, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+earthAnimation_6 = [
+    O, O, O, O, O, O, O, O,
+    O, W, G, W, G, G, O, O,
+    B, B, G, B, B, G, B, O,
+    G, G, G, B, B, B, G, O,
+    G, G, B, B, B, G, G, O,
+    B, B, B, B, G, B, B, O,
+    O, B, B, B, B, B, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+earthAnimation_7 = [
+    O, O, O, O, O, O, O, O,
+    O, G, G, G, W, G, O, O,
+    W, B, B, G, B, B, G, O,
+    B, G, G, B, B, B, B, O,
+    G, B, G, B, B, B, G, O,
+    B, B, B, B, B, G, B, O,
+    O, W, B, B, B, B, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+earthAnimation_8 = [
+    O, O, O, O, O, O, O, O,
+    O, W, G, G, G, W, O, O,
+    W, W, B, B, G, B, B, O,
+    G, B, G, G, B, B, B, O,
+    B, G, B, G, B, B, B, O,
+    B, B, B, B, B, B, G, O,
+    O, W, W, B, B, B, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+earthAnimation_8 = [
+    O, O, O, O, O, O, O, O,
+    O, W, W, G, G, G, O, O,
+    B, W, W, B, B, G, B, O,
+    B, G, B, G, G, B, B, O,
+    G, G, B, B, B, G, B, O,
+    B, B, B, B, B, B, B, O,
+    O, W, W, W, B, B, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+earthAnimation_8 = [
+    O, O, O, O, O, O, O, O,
+    O, W, W, W, G, G, O, O,
+    B, B, W, W, B, B, G, O,
+    B, B, G, B, G, G, B, O,
+    G, G, G, B, B, B, G, O,
+    B, B, B, B, B, B, B, O,
+    O, W, W, W, W, B, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+earthAnimation_end = [
+    O, O, O, O, O, O, O, O,
+    O, B, W, W, W, G, O, O,
+    G, B, B, W, W, B, B, O,
+    B, G, B, G, B, G, G, O,
+    G, G, B, B, G, B, G, O,
+    B, B, G, B, B, B, B, O,
+    O, W, W, W, B, B, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
 #Create the animation
 while True: 
     sense.set_pixels(earthAnimation_1)
@@ -63,4 +140,14 @@ while True:
     sense.set_pixels(earthAnimation_3)
     time.sleep(1)
 	sense.set_pixels(earthAnimation_4)
+    time.sleep(1)
+	sense.set_pixels(earthAnimation_5)
+    time.sleep(1)
+	sense.set_pixels(earthAnimation_6)
+    time.sleep(1)
+	sense.set_pixels(earthAnimation_7)
+    time.sleep(1)
+	sense.set_pixels(earthAnimation_8)
+    time.sleep(1)
+	sense.set_pixels(earthAnimation_9)
     time.sleep(1)
--- calculate_gps03.py
+++ calculate_gps03.py
@@ -1,49 +1,49 @@
-#Version 3
-#Print out to Sense Hat
+# Version 3
+# Print out to Sense Hat
 
-#Import SenseHat
+# Import SenseHat
 from sense_hat import SenseHat
 sense = SenseHat()
 import ephem
 import time
 
-#ISS Two Line telemetry
+# ISS Two Line telemetry
 name = "ISS"
 line1 = "1 25544U 98067A   17320.58149788  .00003295  00000-0  56951-4 0  9992"
 line2 = "2 25544  51.6424 358.6134 0004424 120.6220  11.4433 15.54157095 85451"
 
-#Calculate the ISS location using ephem and pass that back to ISS variable
+# Calculate the ISS location using ephem and pass that back to ISS variable
 ISS = ephem.readtle(name, line1, line2)
 ISS.compute()
 
-#Convert to Strings
+# Convert to Strings
 ISSlongString = str(ISS.sublong)
 ISSlatString = str(ISS.sublat)
 
 ISSlong = ISSlongString.split(":")
 ISSlat = ISSlatString.split(":")
 
-#Convert to floating point numbers for checking ranges
+# Convert to floating point numbers for checking ranges
 ISSlong[0] = float(ISSlong[0])
 ISSlat[0] = float(ISSlat[0])
 
 print ISSlong[0]
 print ISSlat[0]
 
-#Main loop to run and keep checking position
+# Main loop to run and keep checking position
 while True:
-	#Is the ISS between these positions?
+	# Is the ISS between these positions?
 	if(ISSlong[0] >= -100 and ISSlong[0] <= -7) and (ISSlat[0] >= -400 and ISSlat[0] <= -1):
 		
-		#If so print "YAY!" and location
+		#I f so print "YAY!" and location
 		sense.show_message("Yay! I found the ISS!")
 		print ISS.sublong, ISS.sublat
 		
-		#Sleep and recalculate
+		# Sleep and recalculate
 		time.sleep(5)
 		ISS.compute()
 	
-	#If not then do this set of commands print "Where?", location, sleep and recalculate
+	# If not then do this set of commands print "Where?", location, sleep and recalculate
 	else:
 		print ("Where on Earth is the ISS?")
 		print ISS.sublong, ISS.sublat
