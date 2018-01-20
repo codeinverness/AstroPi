c7ec08bda1c09094fef6cab9833323c02b07a79e
Unknown <daryl.titcomb@swarthoull.com>
2018-01-20 21:19:40 +0000

    Get most common colour
    added a function to get the most common colour

--- DEV/Get_center_100x100_avg.py
+++ DEV/Get_center_100x100_avg.py
@@ -1,12 +1,23 @@
 #This code takes an image and returns a value for the average of the center 100x100 pixels
 #
 #Revision 1 - 2018-01-20 - Inital Commit
+#
+# https://zeevgilovitz.com/detecting-dominant-colours-in-python
 
 from picamera import PiCamera
 from time import sleep
 from PIL import Image
 dir = '/home/pi/tmp/test.jpg'
 
+def most_frequent_color(image):
+    w, h = image.size
+    pixels = image.getcolors(w * h)
+    most_frequent_pixel = pixels[0]
+    for count, colour in pixels:
+        if count > most_frequent_pixel[0]:
+            most_frequent_pixel = (count, colour)
+    compare("Most Common", image, most_frequent_pixel[1])
+    return most_frequent_pixel
 
 camera = PiCamera()
 camera.resolution = (2592, 1944) #set the resolution of the camera to as big as possible
@@ -22,5 +33,7 @@ croppedimage = openimage.crop(cropbox) #take only the center 100x100 pixels
 dir = '/home/pi/tmp/test2.jpg' #DEBUG set file path to a new name
 croppedimage.save(dir) #DEBUG save the image to a file
 openimage = Image.open(dir) #DEBUG reopen the image
-print(openimage.getcolors(25555)) #Return a list of colours
+
+print(most_frequent_color(openimage))
+
 exit()
