5176ef3230e6ae1fc2f11036d0947261fcb2399c
Unknown <daryl.titcomb@swarthoull.com>
2018-01-20 20:04:51 +0000

    Comments/resolutions
    Set the resolution of the camera and added comments doe /DEV channel

--- DEV/Get_center_100x100_avg.py
+++ DEV/Get_center_100x100_avg.py
@@ -5,19 +5,20 @@
 from picamera import PiCamera
 from time import sleep
 from PIL import Image
+dir = '~/tmp/test.bmp'
+
 
 camera = PiCamera
+camera.resolution(2592, 1944) #set the resolution of the camera to as big as possible
+camera.framerate(15 #set the cameras framerate
+camera.start_preview() #start the viewer
+sleep(5) #wait for the image to stabalise
+camera.capture(dir) #take an image and save to working directory
 
-camera.resolution(2592, 1944)
-camera.start_preview()
-sleep(5)
-dir = '~/tmp/test.bmp'
-camera.capture(dir)
-
-Image.open(dir)
-Image.crop(1246,922,1346,1022)
-dir = '~/tmp/test2.bmp'
-Image.save(dir)
-Image.open(dir)
-print(Image.getcolours(25555))
+Image.open(dir) #open the image in pillow
+Image.crop(1246,922,1346,1022) #take only the center 100x100 pixels
+dir = '~/tmp/test2.bmp' #DEBUG set file path to a new name
+Image.save(dir) #DEBUG save the image to a file
+Image.open(dir) #DEBUG reopen the image
+print(Image.getcolours(25555)) #Return a list of colours
 exit()
