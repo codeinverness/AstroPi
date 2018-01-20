c6384e68a932a1d29263a61dfd701b27c25a867f
Unknown <daryl.titcomb@swarthoull.com>
2018-01-20 20:37:49 +0000

    /DEV debug
    debug of /DEV tree

--- DEV/Get_center_100x100_avg.py
+++ DEV/Get_center_100x100_avg.py
@@ -5,20 +5,22 @@
 from picamera import PiCamera
 from time import sleep
 from PIL import Image
-dir = '~/tmp/test.bmp'
+dir = '/home/pi/tmp/test.jpg'
 
 
-camera = PiCamera
-camera.resolution(2592, 1944) #set the resolution of the camera to as big as possible
-camera.framerate(15 #set the cameras framerate
+camera = PiCamera()
+camera.resolution = (2592, 1944) #set the resolution of the camera to as big as possible
+camera.framerate = 15 #set the cameras framerate
 camera.start_preview() #start the viewer
 sleep(5) #wait for the image to stabalise
 camera.capture(dir) #take an image and save to working directory
+camera.stop_preview()
 
-Image.open(dir) #open the image in pillow
-Image.crop(1246,922,1346,1022) #take only the center 100x100 pixels
-dir = '~/tmp/test2.bmp' #DEBUG set file path to a new name
-Image.save(dir) #DEBUG save the image to a file
-Image.open(dir) #DEBUG reopen the image
-print(Image.getcolours(25555)) #Return a list of colours
+openimage = Image.open(dir) #open the image in pillow
+cropbox = (1246,922,1346,1022)
+croppedimage = openimage.crop(cropbox) #take only the center 100x100 pixels
+dir = '/home/pi/tmp/test2.jpg' #DEBUG set file path to a new name
+croppedimage.save(dir) #DEBUG save the image to a file
+openimage = Image.open(dir) #DEBUG reopen the image
+print(openimage.getcolors(25555)) #Return a list of colours
 exit()
