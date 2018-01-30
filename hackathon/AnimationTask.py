cb0bdaf17824904f2b099e3e10602ceb4025266c
Code Inverness <35268908+codeinverness@users.noreply.github.com>
2018-01-30 23:28:20 +0000

    More Tasks for the hackathon

---
+++ hackathon/AnimationTask.py
@@ -0,0 +1,6 @@
+# The Task: Crop image
+# https://github.com/raspberrypilearning/astro-pi-guide/blob/master/files/Sense-HAT-Worksheet-Digital.pdf
+# Get worksheet from a mentor :)
+
+from sense_hat import SenseHat
+sense = SenseHat()
---
+++ hackathon/CameraTask.py
@@ -0,0 +1,5 @@
+# The Task: Write data from sensors to file
+# https://projects.raspberrypi.org/en/projects/sense-hat-data-logger
+
+from sense_hat import SenseHat
+sense = SenseHat()
