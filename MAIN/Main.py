b23b43a395ff30e283ce92c7555dc49e5bc24c69
Daryl Titcomb <daryl.titcomb@swarthoull.com>
2018-01-31 22:31:16 +0000

    work on main.py

--- MAIN/Main.py
+++ MAIN/Main.py
@@ -5,17 +5,17 @@
 # most_frequent_color and average_colour used by permission from:
 # https://github.com/ZeevG/python-dominant-image-colour
 
-# Start Camera
+# Start Camera    **DONE**
 # Setup CSV and write headers
-# Display "Ready"
+# Display "Ready"    **DONE**
 # Open Loop
 #   Show Picture
-#   Setup Image File Names using imagefile (imgdir1, imgdir2)
-#   Read Sensors
-#   Take Image
-#   Save image as imgdir1
-#   open immage and proceess, saving the cropped image as imgdir2
-#   Build String and append to file
+#   Setup Image File Names using imagefile (imgdir1, imgdir2)    **DONE**
+#   Read Sensors    **DONE**
+#   Take Image    **DONE**
+#   Save image as imgdir1    **DONE**
+#   open immage and proceess, saving the cropped image as imgdir2    **DONE**
+#   Build String and append to file    **DONE**
 #   carry out annimation
 
 
@@ -27,10 +27,21 @@ from time import sleep
 from PIL import Image
 from sense_hat import SenseHat
 
-dir = '/home/pi/tmp/test.jpg'
-imagefile = 10001
+imagefile = 10000
+imagename = ''
+imagename2 = ''
 timestamp = datetime.datetime.now()
 datafile = ''
+whatispicture = ''
+logfile = "log.csv"
+green = (0, 255, 0)
+
+landref = "land.txt"
+landtupples = []
+searef = "sea.txt"
+seatupples = []
+cloudref "cloud.txt"
+cloudtupples = []
 
 
 def most_frequent_color(image):
@@ -59,14 +70,78 @@ def startcamera():
     camera.start_preview() #start the viewer
     sleep(5) #wait for the image to stabalise
 
-startcamera()
+def takepicture():
+	camera.capture(imagename) #take an image and save to working directory
+	openimage = Image.open(imagename) #open the image in pillow
+	cropbox = (1246,922,1346,1022) #define the center 100x100 pixels
+	croppedimage = openimage.crop(cropbox) #take only the center 100x100 pixels
+	croppedimage.save(imagename2) #save the center 100x100 as a new file
+	openimage = Image.open(imagename2) #reopen the same file
 
-camera.capture(dir) #take an image and save to working directory
-openimage = Image.open(dir) #open the image in pillow
-cropbox = (1246,922,1346,1022)
-croppedimage = openimage.crop(cropbox) #take only the center 100x100 pixels
-croppedimage.save(dir) #DEBUG save the image to a file
-openimage = Image.open(dir) #DEBUG reopen the image
+def nextimagename():
+	imagefile = imagefile+1
+	imagename = str(imagefile)+"_HR.jpg"
+	imagename2  = str(imagefile)+"_100x100.jpg"
+
+def getlogdata():
+	sense_data=[]
+	sense_data.append(sense.get_temperature())
+	sense_data.append(sense.get_humidity())
+	sense_data.append(sense.get_pressure())
+
+	o = sense.get_orientation()
+	yaw = o["yaw"]
+	pitch = o["pitch"]
+	roll = o["roll"]
+	sense_data.extend([pitch,roll,yaw])
+
+	mag = sense.get_compass_raw()
+	mag_x = mag["x"]
+	mag_y = mag["y"]
+	mag_z = mag["z"]
+	sense_data.extend([mag_x,mag_y,mag_z])
+
+	acc = sense.get_accelerometer_raw()
+	x = acc["x"]
+	y = acc["y"]
+	z = acc["z"]
+	sense_data.extend([x,y,z])
+
+	gyro = sense.get_gyroscope_raw()
+	gyro_x = gyro["x"]
+	gyro_y = gyro["y"]
+	gyro_z = gyro["z"]
+	sense_data.extend([gyro_x,gyro_y,gyro_z])
+
+	sense_data.append(datetime.datetime.now())
+	return sense_data
+	
+def savelogdata():
+	sense_data = str(getlogdata())
+	sense_data = sense_data[1:-1]
+	sense_data = str(imagefile)+", "+sense_data+", "+whatispicture
+	print(sense_data)
+	fh = open(logfile, “w”) 
+	fh.write(sense_data)
+	fh.close() 
+	
+def earthsea():
+	
+def idleanimation():
+
+
+startcamera()
+sense.show_message("Ready!", text_colour = green)
+
+file = open(landref, “r”) 
+for line in file: 
+	landtupples.append(line) 
+file = open(searef, “r”) 
+for line in file: 
+	seatupples.append(line) 	
+file = open(cloudref, “r”) 
+for line in file: 
+	cloudtupples.append(line) 
 
 #print(most_frequent_color(openimage))
 #print(average_color(openimage))
