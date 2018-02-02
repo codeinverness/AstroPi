d42554c0bf9cfa7cedf7f2bf63d439e4dd74428d
Code Inverness <35268908+codeinverness@users.noreply.github.com>
2018-02-02 21:54:26 +0000

    Update CameraTask.py

--- hackathon/CameraTask.py
+++ hackathon/CameraTask.py
@@ -1,4 +1,6 @@
 # The Task: Start the camera connected to the Raspberry Pi and take a picture
+# Picture should be maximum resolution
+# Set the camera framerate to 15
 
 # https://projects.raspberrypi.org/en/projects/getting-started-with-picamera
 
