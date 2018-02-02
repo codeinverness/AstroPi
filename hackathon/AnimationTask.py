2ca14223ab59481d26b4591e2f63d051b7fe5aa7
Code Inverness <35268908+codeinverness@users.noreply.github.com>
2018-02-02 21:50:00 +0000

    Update AnimationTask.py

--- hackathon/AnimationTask.py
+++ hackathon/AnimationTask.py
@@ -1,4 +1,5 @@
-# The Task: Crop image
+# The Task: Create an animation that can run on the sense hat LED display
+
 # https://github.com/raspberrypilearning/astro-pi-guide/blob/master/files/Sense-HAT-Worksheet-Digital.pdf
 # Get worksheet from a mentor :)
 
