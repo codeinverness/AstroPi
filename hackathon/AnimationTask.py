f418dfbb15089dbaf03f97d33c3a8b439dc59d65
Code Inverness <35268908+codeinverness@users.noreply.github.com>
2018-02-02 23:45:19 +0000

    Update AnimationTask.py

--- hackathon/AnimationTask.py
+++ hackathon/AnimationTask.py
@@ -1,4 +1,5 @@
 # The Task: Create an animation that can run on the sense hat LED display
+# the animation should use 8 images!
 
 # https://github.com/raspberrypilearning/astro-pi-guide/blob/master/files/Sense-HAT-Worksheet-Digital.pdf
 # Get worksheet from a mentor :)
