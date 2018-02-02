0a5ac2e802f4648a3cae64ce0f292358d4012190
Code Inverness <35268908+codeinverness@users.noreply.github.com>
2018-02-02 22:00:16 +0000

    Create CropImageTask.py

---
+++ hackathon/CropImageTask.py
@@ -0,0 +1,9 @@
+# Task: Crop the middle 100 x 100 pixels
+# You will need to use the PIL Image functions
+# https://pillow.readthedocs.io/en/latest/reference/Image.html
+
+from PIL import Image
+from sense_hat import SenseHat
+sense = SenseHat()
+
+sense.show_message("Team Name", text_colour = green)
