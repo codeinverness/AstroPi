92820ed6e0baf7298ac0a22e8215413588e199fc
Code Inverness <35268908+codeinverness@users.noreply.github.com>
2018-02-03 07:28:39 +0000

    Update and rename DataLogTask.py to GetTemperature.py

--- hackathon/DataLogTask.py
+++
@@ -1,6 +0,0 @@
-# The Task: Create an anomation to run on the Sense Hat
-# https://github.com/raspberrypilearning/astro-pi-guide/blob/master/files/Sense-HAT-Worksheet-Digital.pdf
-# Get worksheet from a mentor :)
-
-from sense_hat import SenseHat
-sense = SenseHat()
---
+++ hackathon/GetTemperature.py
@@ -0,0 +1,5 @@
+# The Task: Capture the Temperature from the sense hat
+# https://github.com/raspberrypilearning/astro-pi-guide/blob/master/sensors/temperature.md
+
+from sense_hat import SenseHat
+sense = SenseHat()
